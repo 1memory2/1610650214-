26ddfb797cc1410c5098a46664d25e7f18bfd410
1memory2 <674754010@qq.com>
2018-06-17 20:43:59 +0800

    做了修改

--- hw01/main.py
+++ hw01/main.py
@@ -1,9 +1,11 @@
 <html>
 <body>
 
-<table width="500" border="0">
+
+</body>
+<table width="200" border="200">
 <tr>
-<td colspan="2" style="background-color:#99bbbb;">
+<td colspan="3" style="background-color:#99bbbb;">
 <h1>Main Title of Web Page</h1>
 </td>
 </tr>
@@ -17,13 +19,17 @@ JavaScript
 </td>
 <td style="background-color:#EEEEEE;height:200px;width:400px;text-align:top;">
 Content goes here</td>
+<td style="background-color:#ffff99;width:100px;text-align:top;">
+<b>BANJI</b><br />
+XINXI<br />
+1602<br />
+HanXiao
+</td>
 </tr>
 
 <tr>
-<td colspan="2" style="background-color:#99bbbb;text-align:center;">
+<td colspan="3" style="background-color:#99bbbb;text-align:center;">
 Copyright W3School.com.cn</td>
 </tr>
 </table>
-
-</body>
 </html>
