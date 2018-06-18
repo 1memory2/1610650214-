e5b19b605ada2e6df47c5009b56ba92b4fb7ec8c
1memory2 <674754010@qq.com>
2018-06-18 21:13:48 +0800

    乘法表

--- hw02/main.py
+++ hw02/main.py
@@ -1,40 +1,35 @@
-<html lang="en">
+<html>
 
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title>Document</title>
-<script type="text/javascript">
-function getValue() {
+    <meta charset="utf-8">
+    <title>乘法表</title>
 
-var inputNum = document.getElementById("d1").value;
-var d2 = document.getElementById("d2");
-            
-var tab = "<table border='1' >";
-                
-for (var i = 1; i <= inputNum; i++) {
-tab += "<tr>";
-                   
-for (var j = 1; j <= inputNum; j++) {
-tab +=
-"<td style='background-color:rgb(86, 220, 30);width:80px;height:80px;margin:10px;'></td>";
-}
-tab += "</tr>";
-}
-tab += "</table>"
-d2.innerHTML = tab;
-            
-}
-</script>
+    <script type="text/javascript">
+    function getvalue(){
+    var n = document.getElementById("a1").value;
+    if (n > 0 && n < 10) {
+    document.write("<table style=background-color:#ffff99;width:500px;height:500px;'>");//这里需要一个table标签，样式表可以直接写标签后面
+    for (var i = 1;i<=n;i++) {
+    document.write('<tr>');
+    for (var j = 1; j <= i; j++){
+    document.write('<td style="border:#CC0000 1px solid;width:20px;height:20px;">'+i+'*'+j+'='+i*j+'</td>'); 
+    }
+    document.write('</tr>');
+    }
+    document.write('</table>');
+    } 
+    else alert("请输入1-9之间的数字！")
+    }
+    </script>
+
+    <style>
+
+    </style>
 </head>
 
 <body>
-
-<input type="text" id="d1" placeholder="输入数字1-9">
-<input type="button" value="点击生成表格" onclick="getValue()">
-<div id="d2" ></div>
-
+    <input type="text" id="a1" placeholder="请输入数字1-9">
+    <input type="button" value="确认" onclick="getvalue()">
 </body>
 
 </html>
